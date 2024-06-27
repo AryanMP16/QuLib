880bea5c7dfa3937b9a41bd3b5efc1a40104b53e
AryanMP16 <aryanlinux123@Alhamdulillah.localdomain>
2024-06-27 00:26:26 -0700

    graphing functionality, makefile updates

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include "complex.h"
 #include "Runge_Kutta_4.h"
 #include <iostream>
+#include "matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
 
 std::pair<double, double> ode_system(double x, double y, double dydx) {
 	double dyPrimedx = -y;
@@ -10,7 +13,6 @@ std::pair<double, double> ode_system(double x, double y, double dydx) {
 
 int main() {
 	try {
-
 		const int length = 100;
 		double y0 = 1;
 		double dydx0 = 0;
@@ -19,79 +21,17 @@ int main() {
 
 		std::pair<double*, double*> results = rk4_paired<length>(ode_system, y0, dydx0, x0, h);
 
-		for (int i = 0; i < length; ++i) {
-			std::cout << x0 + i * h << std::endl;
-		}
-
-		std::cout << "//\n//\n//\n//\n//\n//\nFUNCTION Y:\n";
-
-		for (int i = 0; i < length; ++i) {
-			std::cout << results.first[i] << std::endl;
-		}
-
-		/*std::cout << "//\n//\n//\n//\n//\n//\n";
-
-		for (int i = 0; i < length; ++i) {
-			std::cout << results.second[i] << std::endl;
-		}*/
+		std::vector<double> x;
+		for (int i = 0; i < length; ++i)
+			x.push_back(x0 + i * h);
+		
+		std::vector<double> y(results.first, results.first + length);
+		plt::plot(x, y);
+		plt::save("../saves/output.png");
+		
 
 		delete[] results.first;
 		delete[] results.second;
-
-		//matrix.h and complex.h test cases (all passed)
-
-  //      matrix<complexNumber<int>, 3, 3> m1;
-		//matrix<complexNumber<int>, 3, 3> m2;
-
-		//m1.set(0, 0, complexNumber<int>(0, 1));
-		//m1.set(0, 1, complexNumber<int>(4, 1));
-		//m1.set(0, 2, complexNumber<int>(2, 2));
-
-		//m1.set(1, 0, complexNumber<int>(0, 0));
-		//m1.set(1, 1, complexNumber<int>(9, 1));
-		//m1.set(1, 2, complexNumber<int>(1, 0));
-
-		//m1.set(2, 0, complexNumber<int>(5, 5));
-		//m1.set(2, 1, complexNumber<int>(0, 0));
-		//m1.set(2, 2, complexNumber<int>(3, 0));
-
-		////
-
-		//m2.set(0, 0, complexNumber<int>(4, 0));
-		//m2.set(0, 1, complexNumber<int>(2, 1));
-		//m2.set(0, 2, complexNumber<int>(0, 0));
-
-		//m2.set(1, 0, complexNumber<int>(0, 9));
-		//m2.set(1, 1, complexNumber<int>(0, 1));
-		//m2.set(1, 2, complexNumber<int>(8, 1));
-
-		//m2.set(2, 0, complexNumber<int>(1, 1));
-		//m2.set(2, 1, complexNumber<int>(6, 3));
-		//m2.set(2, 2, complexNumber<int>(3, 5));
-
-		//std::cout << "Complex matrix multiplication:\n";
-		//std::cout << "Matrix 1:\n";
-		//m1.displayMatrix();
-		//std::cout << "Matrix 2:\n";
-		//m2.displayMatrix();
-		//std::cout << "Result:\n";
-		//matrix<complexNumber<int>, 3, 3>m3 = m1 * m2;
-		//m3.displayMatrix();
-
-		//std::cout << "Complex conjugation testing:\n";
-		//for (int i = 0; i < m3.get_rows(); i++) {
-		//	for (int j = 0; j < m3.get_cols(); j++)
-		//		m3(i, j).conj();
-		//}
-		//m3.displayMatrix();
-
-		//std::cout << "Hermitian conjugation testing:\n";
-		//vector<complexNumber<int>, 4> v1;
-		//v1.set(0, complexNumber<int>(1, 1));
-		//v1.set(1, complexNumber<int>(1, 0));
-		//v1.set(2, complexNumber<int>(0, 1));
-		//v1.set(3, complexNumber<int>(0, 0));
-		//dual(v1).displayVector();
 
 		return 0;
 
