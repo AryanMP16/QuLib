ddb5d31cb8a67371adaedb964bf45867a2b0311f
AryanMP16 <aryanlinux123@Alhamdulillah.localdomain>
2024-06-27 01:58:32 -0700

    New test cases for schrodinger equation solver

--- tester/main.cpp
+++ tester/main.cpp
@@ -3,41 +3,20 @@
 #include "Runge_Kutta_4.h"
 #include <iostream>
 #include "matplotlibcpp.h"
+#include "schrodingerSolver.h"
 //ignore any squiggly lines under "matplotlibcpp.h". I swear it works just ignore it.
 
 namespace plt = matplotlibcpp;
 
-//Simple sinusoidal fxn
-// std::pair<double, double> ode_system(double x, double y, double dydx) {
-// 	double dyPrimedx = -y;
-// 	return {dydx, dyPrimedx};
-// }
-
-std::pair<double, double> ode_system(double x, double y, double dydx) {
-	double dyPrimedx = -y-x*x+3;
-	return {dydx, dyPrimedx};
+double U(double x){
+	return 2.0;
 }
 
 int main() {
 	try {
-		const int length = 100;
-		double y0 = 1;
-		double dydx0 = 0;
-		double x0 = 0;
-		double h = 0.1;
-
-		std::pair<double*, double*> results = rk4_paired<length>(ode_system, y0, dydx0, x0, h);
-
-		std::vector<double> x;
-		for (int i = 0; i < length; ++i)
-			x.push_back(x0 + i * h);
 		
-		std::vector<double> y(results.first, results.first + length);
-		plt::plot(x, y);
-		plt::save("../saves/output.png");
-
-		delete[] results.first;
-		delete[] results.second;
+		wavefunction psi(U);
+		psi.solve();
 
 		return 0;
 
