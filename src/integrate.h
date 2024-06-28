3369e1c81455a684f63de43bfd61c0d7b87ec3f1
AryanMP16 <aryanlinux123@Alhamdulillah.localdomain>
2024-06-28 01:22:42 -0700

    got infinite integration working

--- src/integrate.h
+++ src/integrate.h
@@ -31,30 +31,29 @@ double integrate(integrand fxn, double x0, double xEnd, double h) {
     return integral;
 }
 
+//variable transformation
+double transformed_fxn(integrand fxn, double t) {
+    double x = t / (1 - t * t);
+    return fxn(x) * (1 + t * t) / ((1 - t * t) * (1 - t * t));
+}
+
 double integrateOverAllSpace(integrand fxn, double h) {
-    double x0 = std::numeric_limits<double>::min();
-    double xEnd = std::numeric_limits<double>::max();
-    //ensure the number of intervals (n) is even
-    int n = static_cast<int>((xEnd - x0) / h);
+    double integral = 0.0;
+    int n = static_cast<int>(2.0 / h); //number of intervals
     if (n % 2 != 0) {
         n++;
-        h = (xEnd - x0) / n;  //recalculate h to ensure even intervals
+        h = 2.0 / n;  //recalculate h to ensure even intervals
     }
 
-	double integral = fxn(x0);
-
-    int i = 0;
-    #pragma omp parallel for private(i) reduction (+ : integral) //multithreading go brr
-    for (i = 0; i < n; i++) {
-        if (i % 2 == 0)
-            integral += 2 * fxn(x0 + i * h);
-        else
-            integral += 4 * fxn(x0 + i * h);
+    for (int i = 0; i <= n; ++i) {
+        double t = -1.0 + i * h;
+        if (!(std::abs(t) >= 1.0)) {
+            double weight = (i == 0 || i == n) ? 1 : ((i % 2 == 0) ? 2 : 4);
+            integral += weight * transformed_fxn(fxn, t);
+        }
     }
 
-    integral += fxn(xEnd);
-    integral *= h/3.0;
-
+    integral *= h / 3.0;
     return integral;
 }
 
